d8cacd29610dd48a31adc550ba907fc4c5a5afe7
tarektusher33 <tarek.tusher@brainstation-23.com>
2024-05-08 17:51:19 +0600

    Database : PostgreSQL integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { loadEnvFile } from 'process';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -14,6 +15,21 @@ import { loadEnvFile } from 'process';
       envFilePath: '.local.env',
       isGlobal : true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [],
+        synchronize: configService.get<boolean>('DB_SYNC'),
+      }),
+      
+      inject: [ConfigService],
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
