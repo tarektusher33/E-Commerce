4568efd1ab20e9d4aca7acc004e4ca8c4d1c75c5
tarektusher33 <tarek.tusher@brainstation-23.com>
2024-05-21 18:07:33 +0600

    sign up dto validation is complete

--- src/module/auth/dto/signup-cart.dto.ts
+++ src/module/auth/dto/signup-cart.dto.ts
@@ -1,20 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsOptional, IsString } from 'class-validator';
 import { IsCustomEmail } from 'src/common/validators/custom-email.validator';
 
 export class CreateSignUpDto {
   @ApiProperty()
+  @IsString()
   firstName: string;
 
   @ApiProperty()
+  @IsString()
+  @IsOptional()
   lastName: string;
 
   @ApiProperty()
-  @IsCustomEmail({message : 'Please enter a valid email address'})
+  @IsCustomEmail({ message: 'Please enter a valid email address' })
+  @IsString()
   email: string;
 
   @ApiProperty()
   password: string;
 
   @ApiProperty()
+  @IsString()
+  @IsOptional()
+  @Transform(({ value }) => value ?? 'user')
   role: string;
 }
