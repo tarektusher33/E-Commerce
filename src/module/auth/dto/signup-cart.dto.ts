65aa5fd1b3302263077d1497e9f2b89c14573776
tarektusher33 <tarek.tusher@brainstation-23.com>
2024-05-23 12:06:44 +0600

    auth refactor complete

--- src/module/auth/dto/signup-cart.dto.ts
+++ src/module/auth/dto/signup-cart.dto.ts
@@ -4,25 +4,44 @@ import { IsEmail, IsOptional, IsString } from 'class-validator';
 import { IsCustomEmail } from 'src/common/validators/custom-email.validator';
 
 export class CreateSignUpDto {
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Tarek ',
+    type: 'string',
+    description: 'Please enter your first name',
+  })
   @IsString()
   firstName: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Rahman',
+    type: 'string',
+    description: 'Please enter your last name',
+  })
   @IsString()
   @IsOptional()
   lastName: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'tarek@gmail.com',
+    type: 'string',
+    description: 'Please enter your valid email address',
+  })
   @IsCustomEmail({ message: 'Please enter a valid email address' })
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '********',
+    type: 'string',
+    description: 'Please enter your Password',
+  })
   password: string;
 
-  @ApiProperty()
-  @IsString()
-  @IsOptional()
+  @ApiProperty({
+    example: 'user or admin',
+    type: 'string',
+    description: 'Please enter your role',
+  })
+  @IsString() 
   @Transform(({ value }) => value ?? 'user')
   role: string;
 }
