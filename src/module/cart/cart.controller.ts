2ad05a9a1ac5eb06e70660b4625ba2c84aad0bbf
tarektusher33 <tarek.tusher@brainstation-23.com>
2024-05-27 19:40:02 +0600

    cart refactor successfully

--- src/module/cart/cart.controller.ts
+++ src/module/cart/cart.controller.ts
@@ -40,40 +40,14 @@ export class CartController {
     @Request() req,
   ): Promise<ApiResponse<Cart | null>> {
     const userId = this.getUserId(req);
-    if (!userId) {
-      return createResponse<null>(null, 'Please log in', HttpStatus.NOT_FOUND);
-    }
-
-    try {
-      const cart = await this.cartService.createCart(createCartDto, userId);
-      if ('message' in cart) {
-        return createResponse<null>(null, cart.message, HttpStatus.BAD_REQUEST);
-      }
-      if (cart) {
-        return createResponse<Cart>(
-          cart,
-          'Added item successfully in the cart',
-          HttpStatus.OK,
-        );
-      } else {
-        return createResponse<null>(
-          null,
-          'Something went wrong for createing cart',
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-    } catch (error) {
-      return createResponse<null>(
-        null,
-        'Internal Server Error',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-        error.message,
-      );
-    }
+    return await this.cartService.createCart(createCartDto, userId);
   }
 
   @Patch(':id')
-  updateCart(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
+  updateCart(
+    @Param('id') id: string,
+    @Body() updateCartDto: UpdateCartDto,
+  ): Promise<ApiResponse<Cart | null>> {
     return this.cartService.updateCart(+id, updateCartDto);
   }
 
@@ -83,82 +57,13 @@ export class CartController {
     @Request() req,
   ): Promise<ApiResponse<Cart | null>> {
     const userId = this.getUserId(req);
-    if (!userId) {
-      return createResponse<null>(null, 'Please log in', HttpStatus.NOT_FOUND);
-    }
-    try {
-      const cart = await this.cartService.removeCart(+id, userId);
-      if ('message' in cart) {
-        return createResponse<null>(null, cart.message, HttpStatus.NOT_FOUND);
-      }
-      if (cart) {
-        return createResponse<Cart>(
-          cart,
-          'Remove Cart Successfully',
-          HttpStatus.OK,
-        );
-      } else {
-        return createResponse<null>(
-          null,
-          'Something went wrong for removing cart',
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-    } catch (error) {
-      return createResponse<null>(
-        null,
-        'Internal Server Error',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-        error.message,
-      );
-    }
+    return await this.cartService.removeCart(+id, userId);
   }
 
-  @Delete('remove-item/:id')
-  async removeItemFromCart(
-    @Param('id') id: string,
-    @Request() req,
-    @Body() removeCartDto: CreateCartDto,
-  ) : Promise<ApiResponse<Cart | string>>{
-    try {
-      const userId = this.getUserId(req);
-      const cartItem = await this.cartService.removeItemFromCart(
-        removeCartDto,
-        +id,
-        userId,
-      );
-      if('message' in cartItem){
-        return createResponse<null> (
-          null,
-          cartItem.message,
-          HttpStatus.NOT_FOUND
-        )
-      }
-      if(cartItem){
-        return createResponse<Cart> (
-          cartItem,
-          'Cart item removed successfully',
-          HttpStatus.OK
-        )
-      }
-      else{
-        return createResponse<Cart> (
-          null,
-          'Somthing went wrong for removing item',
-          HttpStatus.BAD_REQUEST
-        )
-      }
-    } catch (error) {
-      return createResponse<Cart> (
-        null,
-        'Internal Server Error',
-        HttpStatus.INTERNAL_SERVER_ERROR
-      )
-    }
-  }
 
   @Get()
-  findAllCarts() {
-    return this.cartService.findAllCarts();
+  findAllCarts(@Request() req): Promise<ApiResponse<Cart[] | null>> {
+    const userId = this.getUserId(req);
+    return this.cartService.findAllCarts(userId);
   }
 }
