d2be9a5f9a32f1d16eceb6275086153b5efe81bd
tarektusher33 <tarek.tusher@brainstation-23.com>
2024-06-03 14:44:28 +0600

    changed update product dto

--- src/module/product/dto/update-product.dto.ts
+++ src/module/product/dto/update-product.dto.ts
@@ -1,4 +1,57 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductDto } from './create-product.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto {
+  id: number;
+
+  @ApiProperty({
+    example: 'Product Name',
+    type: 'string',
+    description: 'Please enter your product name',
+  })
+  @IsString()
+  productName: string;
+
+  @ApiProperty({
+    example: '0',
+    type: 'number',
+    description: 'Please enter product price',
+  })
+  @IsNumber()
+  price: number;
+
+  @ApiProperty({
+    example: '0',
+    type: 'number',
+    description: 'Please enter discount price',
+  })
+  @IsNumber()
+  @Transform (({value}) => value ?? 0)
+  discountPrice: number;
+
+  @ApiProperty({
+    example: '0',
+    type: 'number',
+    description: 'Please enter stock quantity',
+  })
+  @IsNumber()
+  stockQuantity: number;
+
+  @ApiProperty({
+    example: 'Product description',
+    type: 'string',
+    description: 'Please enter product description',
+  })
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @ApiProperty({
+    example: 'Product category',
+    type: 'string',
+    description: 'Please enter product category',
+  })
+  @IsString()
+  category: string;
+}
